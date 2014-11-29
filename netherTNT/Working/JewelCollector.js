c8c734d9199c96044cf7b6b6d2bb010655972a54
netherTNT <canghaun@naver.com>
2014-11-29 21:31:28 +0900

    Updated JewelCollecter.js

--- netherTNT/Working/JewelCollector.js
+++ netherTNT/Working/JewelCollector.js
@@ -16,6 +16,8 @@ JEWEL_DIAMOND,
 JEWEL_EMERALD,
 JEWEL_QUARTZ
 ];
+const HORIZONTAL=0;
+const VERTICAL=1;
 var overlappingCheck=false;
 
 var hello="99 108 105 101 110 116 77 101 115 115 97 103 101 40 67 104 97 116 67 111 108 111 114 46 71 82 69 69 78 43 34 74 101 119 101 108 67 111 108 108 101 99 116 111 114 32 83 99 114 105 112 116 32 98 121 32 78 101 116 104 101 114 84 78 84 34 41 59".split(" ");
@@ -93,9 +95,6 @@ check.vertical.next=function(x,y,z){
     return getTile(x,y,z+1);
 };
 var selectedJewel={};
-var breakJewel=function{
-    
-};
 function useItem(x,y,z,item,block)
 {
     if(item==501){
@@ -131,17 +130,40 @@ function useItem(x,y,z,item,block)
             }
         }
     }
-    if(item==0){
+    if(item==502){
         if(!selectedJewel.first){
             selectedJewel.first={block:getTile(x,y,z),x:x,y:y,z:z};
         }
         else{
-            selectedJewel.second={block:getTile(x,y,z),x:x,y:y,z:z};
-            setTile(selectedJewel.first.x,selectedJewel.first.y,selectedJewel.first.z,selectedJewel.second.block);
-            setTile(selectedJewel.second.x,selectedJewel.second.y,selectedJewel.second.z,selectedJewel.first.block);
-            breakJewel(x,y,z);
+			if(Math.abs(selectedJewel.first.x-x)==1||Math.abs(selectedJewel.first.z-z)==1){
+                selectedJewel.second={block:getTile(x,y,z),x:x,y:y,z:z};
+                setTile(selectedJewel.first.x,selectedJewel.first.y,selectedJewel.first.z,selectedJewel.second.block);
+                setTile(selectedJewel.second.x,selectedJewel.second.y,selectedJewel.second.z,selectedJewel.first.block);
+		    	if(selectedJewel.first.x-selectedJewel.second.x!=0){
+			        if(check.horizontal.getCount(check.horizontal.getFirstJewel(x,y,z),y,z).count>=3){
+				        while(true){
+							Level.destroyBlock(x,y,z,false);
+							x--;
+							if(x==check.horizontal.getFirstJewel(x+1,y,z)){
+								break;
+							}
+						}
+			        }
+			    }
+			    if(selectedJewel.first.z-selectedJewel.second.z!=0){
+			    	if(check.vertical.getCount(x,y,check.vertical.getFirstJewel(x,y,z)).count>=3){
+					    while(true){
+							Level.destroyBlock(x,y,z);
+							z--;
+							if(z==check.vertical.getFirstJewel(x,y,z+1)){
+								break;
+							}
+						}
+				    }
+			    }
+			}
             selectedJewel.first={};
-            selectedJewel.second=0;
+            selectedJewel.second={};
         }
     }
 }
