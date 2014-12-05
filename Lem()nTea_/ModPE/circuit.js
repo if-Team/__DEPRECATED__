6def6bcb5791e0386a6005512f64afb3a83760a8
Lem()nTea_ <ska827@naver.com>
2014-12-05 20:49:53 +0900

    Basic switch(WIP)

--- Lem()nTea_/ModPE/circuit.js
+++ Lem()nTea_/ModPE/circuit.js
@@ -1,1 +1,21 @@
- 
+/*
+
+Circuit script by lem()ntea.
+Please do not copy my code.
+
+*/
+
+//Block definations
+//Warning: edting this could make error(s).
+var SWITCH_ENABLED = 22 //Lapis lazuli block
+var SWITCH_DISABLED = 20 //Glass
+var WIRE_ENABLED = 41; //Gold block
+var WIRE_DISABLED = 42; //Iron block
+
+function useItem(x, y, z, b, i, s){
+    if(b == SWITCH_ENABLED){
+        net.zhuoweizhang.mcpelauncher.ScriptManager.nativeSetTile(x, y, z, SWITCH_DISABLED, 0);
+    } else if(b == SWITCH_DISABLED){
+        net.zhuoweizhang.mcpelauncher.ScriptManager.nativeSetTile(x, y, z, SWITCH_ENABLED, 0);
+    }
+}
