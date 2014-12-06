0e48191de665b9196b0a7bf56b06aa4430f9161c
우유맛비누 <milk5590@naver.com>
2014-12-06 13:44:38 +0900

    Fix getTarget()

--- milk0417/EntityMove/src/plugin/AnimalEntity/Cow.php
+++ milk0417/EntityMove/src/plugin/AnimalEntity/Cow.php
@@ -16,19 +16,26 @@ class Cow extends Animal{
     }
 
     public function getTarget(){
+        $target = $this->target;
+        if($target instanceof Player){
+            $slot = $target->getInventory()->getItemInHand();
+            if(!$target->dead and !$target->closed && $this->distance($target) <= 6 and $slot->getID() == Item::WHEAT){
+                return $target;
+            }
+        }
         foreach($this->hasSpawned as $p){
-            if($this->distance($p) > 7.2 or $p->spawned === false or $p->dead === true) continue;
+            if($this->distance($p) > 6 or $p->spawned === false) continue;
             $slot = $p->getInventory()->getItemInHand();
             if ($slot->getID() == Item::WHEAT) {
-                $this->target = $p;
+                $target = $p;
                 break;
             }
         }
-        if(!$this->target instanceof Vector3 or $this->moveTime >= mt_rand(80,500) or ($this->target instanceof Player && $this->target->dead)){
+        if(!$target instanceof Vector3 or $this->moveTime >= mt_rand(400, 800) or ($target instanceof Player && ($target->dead or $target->closed))){
             $this->moveTime = 0;
             $this->target = new Vector3($this->x + mt_rand(-40,40), $this->y, $this->z + mt_rand(-40,40));
         }
-        return $this->target;
+        return $this->target = $target;
     }
 
     public function getName(){
