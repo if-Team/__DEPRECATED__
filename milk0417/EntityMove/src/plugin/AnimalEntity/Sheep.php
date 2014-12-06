7d018e7699667c5c8a2f78b248a04874e40d7a05
우유맛비누 <milk5590@naver.com>
2014-12-06 13:47:45 +0900

    getTarget() 일부수정

--- milk0417/EntityMove/src/plugin/AnimalEntity/Sheep.php
+++ milk0417/EntityMove/src/plugin/AnimalEntity/Sheep.php
@@ -19,9 +19,7 @@ class Sheep extends Animal implements Colorable{
         $target = $this->target;
         if($target instanceof Player){
             $slot = $target->getInventory()->getItemInHand();
-            if(!$target->dead and !$target->closed && $this->distance($target) <= 6 and $slot->getID() == Item::SEEDS){
-                return $target;
-            }
+            if(!$target->dead and !$target->closed && $this->distance($target) <= 6 and $slot->getID() == Item::SEEDS){ return $target;
         }
         foreach($this->hasSpawned as $p){
             if($this->distance($p) > 6 or $p->spawned === false) continue;
@@ -31,7 +29,7 @@ class Sheep extends Animal implements Colorable{
                 break;
             }
         }
-        if(!$target instanceof Vector3 or $this->moveTime >= 400 or ($target instanceof Player && ($target->dead or $target->closed))){
+        if(!$target instanceof Vector3 or $this->moveTime >= mt_rand(400, 800) or ($target instanceof Player && ($target->dead or $target->closed))){
             $this->moveTime = 0;
             $this->target = new Vector3($this->x + mt_rand(-40,40), $this->y, $this->z + mt_rand(-40,40));
         }
