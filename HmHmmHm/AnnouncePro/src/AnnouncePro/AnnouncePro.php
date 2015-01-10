c7837ea54ca7e0c1bad411c58aa445220fe3dabd
hm <hmkuak@naver.com>
2015-01-11 02:27:55 +0900

    Announce 1.3 MultiLanguage Options UPDATE

--- HmHmmHm/AnnouncePro/src/AnnouncePro/AnnouncePro.php
+++ HmHmmHm/AnnouncePro/src/AnnouncePro/AnnouncePro.php
@@ -9,27 +9,47 @@ use pocketmine\command\Command;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\scheduler\CallbackTask;
+use pocketmine\command\PluginCommand;
 
 class AnnouncePro extends PluginBase implements Listener {
 	public $config, $configData;
 	public $callback, $before = 1;
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
-		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+		
+		$this->initMessage ();
+		$this->registerCommand ( $this->get ( "commands-announce" ), "AnnouncePro" );
+		
 		$this->config = new Config ( $this->getDataFolder () . "announce.yml", Config::YAML, [ 
 				"enable" => true,
-				"repeat-time" => 5,
-				"prefix" => "[ 서버 ]",
+				"repeat-second" => 5,
+				"prefix" => $this->get ( "default-prefix" ),
 				"suffix" => "",
 				"announce" => [ ] ] );
 		$this->configData = $this->config->getAll ();
 		$this->callback = $this->getServer ()->getScheduler ()->scheduleRepeatingTask ( new CallbackTask ( [ 
 				$this,
-				"AnnouncePro" ] ), $this->configData ["repeat-time"] * 20 );
+				"AnnouncePro" ] ), $this->configData ["repeat-second"] * 20 );
+		
+		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
 	public function onDisable() {
 		$this->config->setAll ( $this->configData );
 		$this->config->save ();
+	}
+	public function initMessage() {
+		$this->saveResource ( "messages.yml", false );
+		$this->messages = (new Config ( $this->getDataFolder () . "messages.yml", Config::YAML ))->getAll ();
+	}
+	public function get($var) {
+		return $this->messages [$this->messages ["default-language"] . "-" . $var];
+	}
+	public function registerCommand($name, $fallback = "", $description = "", $usage = "") {
+		$commandMap = $this->getServer ()->getCommandMap ();
+		$command = new PluginCommand ( $name, $this );
+		$command->setDescription ( $description );
+		$command->setUsage ( $usage );
+		$commandMap->register ( $fallback, $command );
 	}
 	public function onCommand(CommandSender $player, Command $command, $label, Array $args) {
 		if (! isset ( $args [0] )) {
@@ -37,31 +57,31 @@ class AnnouncePro extends PluginBase implements Listener {
 			return true;
 		}
 		switch ($args [0]) {
-			case "활성화" :
+			case $this->get ( "sub-commands-enable" ) :
 				$this->configData ["enable"] = true;
-				$player->sendMessage ( TextFormat::DARK_AQUA . "공지를 활성화 했습니다 !" );
+				$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "enable-complete" ) );
 				break;
-			case "비활성화" :
+			case $this->get ( "sub-commands-disable" ) :
 				$this->configData ["enable"] = false;
-				$player->sendMessage ( TextFormat::DARK_AQUA . "공지를 비활성화 했습니다 !" );
+				$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "enable-complete" ) );
 				break;
-			case "추가" :
+			case $this->get ( "sub-commands-add" ) :
 				array_shift ( $args );
 				$text = $this->replaceColor ( implode ( " ", $args ) );
 				if ($text == "" or $text == " ") {
-					$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 추가 <번호>" );
+					$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "add-help" ) );
 					break;
 				}
 				$this->configData ["announce"] [] = $text;
-				$player->sendMessage ( TextFormat::DARK_AQUA . "해당 공지를 추가했습니다 !" );
+				$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "add-complete" ) );
 				break;
-			case "삭제" :
+			case $this->get ( "sub-commands-delete" ) :
 				if (! isset ( $args [1] )) {
-					$player->sendMessage ( TextFormat::RED . "/공지 삭제 <번호>" );
+					$player->sendMessage ( TextFormat::RED . $this->get ( "delete-help" ) );
 					break;
 				}
 				if (! is_numeric ( $args [1] )) {
-					$player->sendMessage ( TextFormat::DARK_AQUA . "공지번호는 숫자만 적을 수있습니다 !" );
+					$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "delete-must-number" ) );
 					break;
 				}
 				if (isset ( $this->configData ["announce"] [$args [1]] )) {
@@ -73,43 +93,43 @@ class AnnouncePro extends PluginBase implements Listener {
 						$match_new [] = $this->configData ["announce"] [$aaa [1]];
 					$this->configData ["announce"] = $match_new;
 					unset ( $match_new );
-					$player->sendMessage ( TextFormat::DARK_AQUA . "해당 공지를 삭제했습니다 !" );
+					$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "delete-complete" ) );
 				}
 				break;
-			case "리스트" :
+			case $this->get ( "sub-commands-list" ) :
 				if (isset ( $args [1] ) and is_numeric ( $args [1] )) {
 					$this->AnnounceList ( $player, $args [1] );
 				} else {
 					$this->AnnounceList ( $player );
 				}
 				break;
-			case "반복" :
+			case $this->get ( "sub-commands-repeat" ) :
 				if (! is_numeric ( $args [1] )) {
-					$player->sendMessage ( TextFormat::DARK_AQUA . "시간초는 숫자만 적을 수있습니다 !" );
+					$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "repeat-must-number" ) );
 					break;
 				}
-				$this->configData ["repeat-time"] = $args [1];
+				$this->configData ["repeat-second"] = $args [1];
 				$this->callback->remove ();
 				$this->callback = $this->getServer ()->getScheduler ()->scheduleRepeatingTask ( new CallbackTask ( [ 
 						$this,
-						"AnnouncePro" ] ), $this->configData ["repeat-time"] * 20 );
-				$player->sendMessage ( TextFormat::DARK_AQUA . "해당 시간초만큼 반복을 설정했습니다 !" );
+						"AnnouncePro" ] ), $this->configData ["repeat-second"] * 20 );
+				$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "repeat-complete" ) );
 				break;
-			case "접두사" :
+			case $this->get ( "sub-commands-prefix" ) :
 				if (! isset ( $args [1] )) {
-					$player->sendMessage ( TextFormat::RED . "/공지 접두사 <내용>" );
+					$player->sendMessage ( TextFormat::RED . $this->get ( "prefix-help" ) );
 					break;
 				}
 				$this->configData ["prefix"] = $args [1];
-				$player->sendMessage ( TextFormat::DARK_AQUA . "해당 접두사를 설정했습니다 !" );
+				$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "prefix-complete" ) );
 				break;
-			case "접미사" :
+			case $this->get ( "sub-commands-suffix" ) :
 				if (! isset ( $args [1] )) {
-					$player->sendMessage ( TextFormat::RED . "/공지 접미사 <내용>" );
+					$player->sendMessage ( TextFormat::RED . $this->get ( "suffix-help" ) );
 					break;
 				}
 				$this->configData ["suffix"] = $args [1];
-				$player->sendMessage ( TextFormat::DARK_AQUA . "해당 접미사를 설정했습니다 !" );
+				$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "suffix-complete" ) );
 				break;
 			default :
 				$this->helpPage ( $player );
@@ -119,7 +139,6 @@ class AnnouncePro extends PluginBase implements Listener {
 	}
 	public function AnnouncePro() {
 		if ($this->configData ["enable"] != true) return;
-		// TODO 이전공지일경우 안뜨게 조치
 		if (isset ( $this->configData ["announce"] )) $rand = rand ( 0, count ( $this->configData ["announce"] ) - 1 );
 		if (count ( $this->configData ["announce"] ) > 3) while ( $rand == $this->before )
 			$rand = rand ( 0, count ( $this->configData ["announce"] ) - 1 );
@@ -145,7 +164,7 @@ class AnnouncePro extends PluginBase implements Listener {
 		if ($index_count > $full_index * $once_print) $full_index ++;
 		
 		if ($index <= $full_index) {
-			$player->sendMessage ( TextFormat::DARK_AQUA . "*공지리스트를 표시합니다 ! ({$index}/{$full_index}) 총: {$index_count}개" );
+			$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "now-list-show" ) . " ({$index}/{$full_index}) " . $this->get ( "index_count" ) . ": {$index_count}" );
 			$message = null;
 			for($for_i = $once_print; $for_i >= 1; $for_i --) {
 				$now_index = $index * $once_print - $for_i;
@@ -155,18 +174,17 @@ class AnnouncePro extends PluginBase implements Listener {
 			}
 			$player->sendMessage ( $message );
 		} else {
-			$player->sendMessage ( TextFormat::RED . "해당하는 리스트가 없습니다 !" );
+			$player->sendMessage ( TextFormat::RED . $this->get ( "there-is-no-list" ) );
 			return false;
 		}
 	}
 	public function helpPage(CommandSender $player) {
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 활성화|비활성화 - 공지를 활성|비활성화 합니다" );
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 추가 <내용> - 공지를 추가합니다" );
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 삭제 <번호> - 공지를 삭제합니다" );
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 리스트 <내용> - 공지 리스트를 보여줍니다." );
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 반복 <시간초> - 공지 반복시간을 수정합니다." );
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 색상표 - 공지에 사용가능한 색상표를 출력합니다" );
-		$player->sendMessage ( TextFormat::DARK_AQUA . "/공지 접두사|접미사 <내용>  - 공지 앞뒤에 단어를 추가합니다. " );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "help-1" ) );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "help-2" ) );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "help-3" ) );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "help-4" ) );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "help-5" ) );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $this->get ( "help-6" ) );
 	}
 }
 ?>
