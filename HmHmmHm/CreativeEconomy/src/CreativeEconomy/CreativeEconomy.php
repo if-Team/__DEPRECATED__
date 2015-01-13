5e911609517ecffcb68844c7a2b7fb93121dced8
hm <hmkuak@naver.com>
2015-01-14 02:52:49 +0900

    CreativeEconomy 기본PE시세완성, 이벤트 작업시작

--- HmHmmHm/CreativeEconomy/src/CreativeEconomy/CreativeEconomy.php
+++ HmHmmHm/CreativeEconomy/src/CreativeEconomy/CreativeEconomy.php
@@ -1,9 +1,43 @@
-<?php 
+<?php
+
 namespace CreativeEconomy;
 
-class CreativeEconomy extends PluginBase implements Listner{
-	public function onEnable(){
-		$this->getServe
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\command\PluginCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\block\SignChangeEvent;
+
+class CreativeEconomy extends PluginBase implements Listener {
+	public function onEnable() {
+		$this->saveResource ( "config.yml", false );
+		$this->saveDefaultConfig ();
+		$this->reloadConfig ();
+		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+	}
+	public function onTouch(PlayerInteractEvent $event) {
+	}
+	public function onSign(SignChangeEvent $event) {
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, $args) {
+	}
+	public function get($var) {
+		return $this->messages [$this->messages ["default-language"] . "-" . $var];
+	}
+	public function initMessage() {
+		$this->saveResource ( "messages.yml", false );
+		$this->messages = (new Config ( $this->getDataFolder () . "messages.yml", Config::YAML ))->getAll ();
+	}
+	public function registerCommand($name, $fallback, $permission, $description = "", $usage = "") {
+		$commandMap = $this->getServer ()->getCommandMap ();
+		$command = new PluginCommand ( $name, $this );
+		$command->setDescription ( $description );
+		$command->setPermission ( $permission );
+		$command->setUsage ( $usage );
+		$commandMap->register ( $fallback, $command );
 	}
 }
 
