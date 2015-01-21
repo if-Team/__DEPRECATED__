87b3bec427cd119c7f4e0313839444c9d29d770f
hm <hmkuak@naver.com>
2015-01-22 04:03:02 +0900

    Babel 1.0완성, +KOREAN TRANSLATE MESSAGES

--- HmHmmHm/Babel/src/Babel/Babel.php
+++ HmHmmHm/Babel/src/Babel/Babel.php
@@ -10,26 +10,33 @@ use pocketmine\command\PluginCommand;
 use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\network\protocol\Info;
+use pocketmine\utils\TextFormat;
 
 class Babel extends PluginBase implements Listener {
 	public $messages;
 	public function onEnable() {
 		$this->initMessage ();
-		var_dump ( $this->messages );
-		$this->registerCommand ( $this->get ( "translate-command" ), "babel", $this->get ( "translate-command-description" ), "/" . $this->get ( "translate-command" ) );
+		$this->registerCommand ( $this->get ( "translate-command" ), "babel", $this->get ( "translate-command-description" ), "/" . $this->get ( "translate-command" ), "babel" );
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
 		if (strtolower ( $command->getName () ) == $this->get ( "translate-command" )) {
+			if (! isset ( $args [0] ) or ! isset ( $this->messages ["translation"] [$args [0]] )) {
+				$list = TextFormat::DARK_AQUA . $this->get ( "print-all-list" ) . "\n";
+				foreach ( $this->messages ["translation"] as $index => $text )
+					$list .= $index . " ";
+				$sender->sendMessage ( $list );
+				return true;
+			}
 			$this->makeServerCommand ( $sender, $command, $label, $args );
 			return true;
 		}
 	}
 	private function makeServerCommand(CommandSender $sender, Command $command, $label, array $args) {
 		$server = Server::getInstance ();
-		$pharPath =\pocketmine\DATA . $server->getName () . ".phar"; // . DIRECTORY_SEPARATOR
+		$pharPath =\pocketmine\DATA . $server->getName () . "_translate.phar";
 		if (file_exists ( $pharPath )) {
-			$sender->sendMessage ( "Phar 파일이 이미 존재합니다, 덮어쓰기중..." );
+			$sender->sendMessage ( $server->getName () . "_translate.phar" . $this->get ( "phar-already-exist" ) );
 			@unlink ( $pharPath );
 		}
 		$phar = new \Phar ( $pharPath );
@@ -52,15 +59,32 @@ class Babel extends PluginBase implements Listener {
 					$filePath ), array (
 					"/",
 					"" ), $file ), "/" );
-			if ($path {0} === "." or strpos ( $path, "/." ) !== false or substr ( $path, 0, 4 ) !== "src/") {
-				continue;
+			if ($path {0} === "." or strpos ( $path, "/." ) !== false or substr ( $path, 0, 4 ) !== "src/") continue;
+			echo "추가 중... " . ($file->getFilename ()) . "\n";
+			foreach ( $this->messages ["translation"] [$args [0]] as $index => $phpfile ) {
+				if ($file->getFilename () == $index) {
+					$translate = file_get_contents ( $file ); // TODO
+					foreach ( $this->messages ["translation"] [$args [0]] [$file->getFilename ()] as $index => $text ) {
+						$translate = str_replace ( $index, $text, $translate );
+						echo "변경완료 [{$index}] [{$text}]\n";
+					}
+					if (! file_exists ( \pocketmine\DATA . "extract/" . explode ( $file->getFilename (), $path )[0] )) mkdir (\pocketmine\DATA . "extract/" . explode ( $file->getFilename (), $path )[0], 0777, true );
+					echo "폴더생성 " . \pocketmine\DATA . "extract/" . explode ( $file->getFilename (), $path )[0] . "\n";
+					file_put_contents (\pocketmine\DATA . "extract/" . $path, $translate );
+					break;
+				}
 			}
-			$phar->addFile ( $file, $path );
+			if (file_exists ( \pocketmine\DATA . "extract/" . $path )) {
+				$phar->addFile ( \pocketmine\DATA . "extract/" . $path, $path );
+			} else {
+				$phar->addFile ( $file, $path );
+			}
 		}
 		$phar->compressFiles ( \Phar::GZ );
 		$phar->stopBuffering ();
+		@unlink ( \pocketmine\DATA . "extract/" );
 		
-		$sender->sendMessage ( "PocketMine-MP Phar 파일이 생성되었습니다 " . $pharPath );
+		$sender->sendMessage ( $server->getName () . "_translate.phar" . $this->get ( "phar-translate-complete" ) . $pharPath );
 		
 		return true;
 	}
@@ -71,11 +95,11 @@ class Babel extends PluginBase implements Listener {
 	public function get($var) {
 		return $this->messages [$this->messages ["default-language"] . "-" . $var];
 	}
-	public function registerCommand($name, $fallback = "", $description = "", $usage = "") {
+	public function registerCommand($name, $fallback, $description, $usage, $permission) {
 		$commandMap = $this->getServer ()->getCommandMap ();
 		$command = new PluginCommand ( $name, $this );
 		$command->setDescription ( $description );
-		$command->setPermission ( "announcepro" );
+		$command->setPermission ( $permission );
 		$command->setUsage ( $usage );
 		$commandMap->register ( $fallback, $command );
 	}
