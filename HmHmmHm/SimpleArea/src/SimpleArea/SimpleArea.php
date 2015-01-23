fdc46a4d0bff9476358f69cfb5fa5c3f3f91a405
hm <hmkuak@naver.com>
2015-01-23 17:07:08 +0900

    SimpleArea messages 오타수정, 울타리관련 옵션 제거

--- HmHmmHm/SimpleArea/src/SimpleArea/SimpleArea.php
+++ HmHmmHm/SimpleArea/src/SimpleArea/SimpleArea.php
@@ -1115,7 +1115,6 @@ class SimpleArea extends PluginBase implements Listener {
 			$this->message ( $player, "다른 유저의 영역과 겹칩니다, 설정불가 !" );
 		} else {
 			foreach ( $this->config_Data ["default-protect-blocks"] as $protect_block )
-				$this->db [$player->level->getFolderName ()]->addOption ( $area_id, $protect_block );
 			$this->message ( $player, "성공적으로 집을 구매했습니다 !" );
 			if ($this->checkEconomyAPI ()) {
 				$this->economyAPI->reduceMoney ( $player, $this->config_Data ["economy-home-price"] );
